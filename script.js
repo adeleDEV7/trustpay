cbf5aba25e76b7f31e8af1052de4023e2211460b
adeleDEV7 <adeledeveloper7@gmail.com>
2024-08-04 10:27:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("applicationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            alert("Thank you for your application! We will get back to you soon.");
+            form.reset();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert("Oops! There was a problem submitting your form");
+                }
+            });
+        }
+    }).catch(error => {
+        alert("Oops! There was a problem submitting your form");
+    });
+});
